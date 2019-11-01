fe0bbc7e20184d7c9af64aaf7e44fd278eaed3b8
Manuel <info@toolwheel.de>
2019-11-01 11:59:37 +0100

    Added AfmuRepairs event

---
+++ src/EliteEventAPI.Core/EliteEventAPI/Services/Events/AfmuRepairsEvent.cs
@@ -0,0 +1,21 @@
+using Newtonsoft.Json;
+
+namespace EliteEventAPI.Services.Events
+{
+    public partial class AfmuRepairsEvent : EventModelBase
+    {
+        public override string Eventname => "AfmuRepairs";
+
+        [JsonProperty]
+        public string Module { get; internal set; }
+
+        [JsonProperty("Module_Localised")]
+        public string ModuleLocalised { get; internal set; }
+
+        [JsonProperty]
+        public bool FullyRepaired { get; internal set; }
+
+        [JsonProperty]
+        public double Health { get; internal set; }
+    }
+}
