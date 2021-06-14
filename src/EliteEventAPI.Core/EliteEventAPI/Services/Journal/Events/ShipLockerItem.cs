b732feaacf22a355da124a5b68ed3a0e778110b4
manuel <info@toolwheel.de>
2021-06-14 11:54:40 +0200

    Fixed ShipLocker event.

---
+++ src/EliteEventAPI.Core/EliteEventAPI/Services/Journal/Events/ShipLockerItem.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace EliteEventAPI.Services.Journal.Events
+{
+    public class ShipLockerItem
+    {
+        [JsonProperty]
+        public string Name { get; internal set; }
+
+        [JsonProperty("Name_Localised")]
+        public string NameLocalised { get; internal set; }
+
+        [JsonProperty]
+        public long OwnerId { get; internal set; }
+
+        [JsonProperty]
+        public int Count { get; internal set; }
+    }
+}
